1bf17c5ef738ae3fde84d655a758c02770a2d50e
Juan Diego <silgajuandiego@gmail.com>
2019-05-13 11:05:52 -0500

    [Add] Add update and delete clients

--- platzi-ventas/clients/commands.py
+++ platzi-ventas/clients/commands.py
@@ -49,19 +49,49 @@ def list(ctx): ## funcion list solo recibe el contexto que un diccionario vacio
   for client in client_list: ## iteramos la lista de los clientes
     click.echo(f'''{client['uid']} | {client['name']} | {client['company']} | {client['email']} | {client['position']}''')
 
-
+##**** Interfaz de consola para la actualización de clientes******
 @clients.command()
+@click.argument('client_uid', ## argumentos
+                type=str)
 @click.pass_context
-def update(ctx, client_uid): ## función update
-  """Updates a Client"""
-  pass
+def update(ctx, client_uid):
+  """Updates a single Client"""
+  client_service = ClientService(ctx.obj['clients_table'])
+
+  ## client_list = client_service.list_clients()
+
+  client = [client for client in client_service.list_clients() if client['uid'] == client_uid]
+
+  if client:
+    client = _update_client_flow(Client(**client[0]))
+    client_service.update_client(client)
+
+    click.echo('Client updated')
+  else:
+    click.echo('Client not found')
 
 
+def _update_client_flow(client): ## el flujo de comandos para actualizar
+  click.echo('Leave empty if you don\'t want to modify the value')
+
+  client.name = click.prompt('New name', type=str, default=client.name)
+  client.company = click.prompt('New company', type=str, default=client.company)
+  client.email = click.prompt('New email', type=str, default=client.email)
+  client.position = click.prompt('New position', type=str, default=client.position)
+
+  return client
+
+##**** Interfaz de consola para borrar de clientes******
 @clients.command()
+@click.argument('client_uid',
+                type=str)
 @click.pass_context
-def delete(ctx, client_uid): ## función Delete
+
+def delete(ctx, client_uid):
   """Deletes a client"""
-  pass
+  client_service = ClientService(ctx.obj['clients_table'])
 
+  if click.confirm(f'Are you sure you want to delete the client with uid: {client_uid}'):
+    client_service.delete_client(client_uid)
 
-all = clients ## definimos un alias para que sea fácil declarar todas estas funciones en un solo code
+all = clients
--- platzi-ventas/clients/services.py
+++ platzi-ventas/clients/services.py
@@ -1,6 +1,8 @@
 import csv
+import os
 
 from clients.models import Client
+
 
 class ClientService: ## declaramos la clase logica de negocio
 
@@ -19,3 +21,35 @@ class ClientService: ## declaramos la clase logica de negocio
       reader = csv.DictReader(f, fieldnames=Client.schema()) ## solo leemos el archivo
 
       return list(reader) ## y lo retornamos como una lista para que sea leído
+
+
+  ##Lógica para actualizar un cliente
+  def update_clients(self, updated_client): ## recibe como parametro self segundo parametro un cliente que ya está actualizado
+    clients = self.list_clients() ## obtenemos una lista de clientes de la lista que ya tenemos
+
+    updated_clients = [] ## variable auxiliar para el ciclo y solo tomar al cliente que se modificó y los demás dejarlos como se modificó
+    for client in clients: ## por cado cliente en los clientes
+      if client['uid'] == updated_client.uid: ## si su iud es igual al updated_client.uid
+        updated_clients.append(updated_client.to_dict()) ## añadimos el cliente que ha sido actualizado con el método to_dict para que python csv diccionarios y no objetos to_dict() lo convierte a diccionario
+      else:
+        updated_clients.append(client) ## si el cliente no ha sido actualizado simplemente se mete como está
+
+    self._save_to_disk(updated_clients) ## guardar en disco
+
+## Logica para borrar un cliente****
+    def delete_client(self, client_uid):
+        clients = self.list_clients()
+        updated_clients = [client for client in clients if client['uid'] != client_uid]
+
+        self._save_to_disk(updated_clients)
+
+
+  def _save_to_disk(self, clients): ## metodo privado
+    tmp_table_name = self.table_name + '.tmp' ## creamos un archivo temporal porque ya arbimos el archivo porque lo abrimos en modo lectura cuando obtuvimos nuestra lista de clientes
+    with open(tmp_table_name, mode='w') as f: ## abrimos el archivo temporal
+      writer = csv.DictWriter(f, fieldnames=Client.schema()) ## comenzamos a declarar nuestro writer
+      writer.writerows(clients) ## escribimos todas las filas
+
+    os.remove(self.table_name) ## removemos la tabla original
+    os.rename(tmp_table_name, self.table_name) ## renombramos la tabla temporal con el nombre original de la tabla
+
